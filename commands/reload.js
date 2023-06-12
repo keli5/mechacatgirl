1f7192b15ad958f13e71fe8b8b784061a9c89c71
letters <46039037+keli5@users.noreply.github.com>
2023-06-12 12:56:23 -0500

    fix wrong param name

--- commands/reload.js
+++ commands/reload.js
@@ -1,7 +1,12 @@
+const util = require("../util/nicknames")
+
 module.exports = {
     name: 'reload',
     description: 'example command',
-    async execute(bot, message, args, raw) {
+    async execute(bot, message, args, senderNickname) {
+        owner = await util.nickIsBotOwner(bot, senderNickname)
+        if (!owner) return bot.chat("&cYou must be owner to use this command!")
+
         bot.commands.clear()
         cmds = await bot.loadCommands(bot)
 
