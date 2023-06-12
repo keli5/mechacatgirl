ff0da81cdc0a0ddd21d7228309e3db3e2e6e668b
letters <46039037+keli5@users.noreply.github.com>
2023-06-12 11:48:16 -0500

    better info.js

--- commands/info.js
+++ commands/info.js
@@ -1,7 +1,18 @@
+const execSync = require("child_process").execSync
+
 module.exports = {
     name: 'info',
     description: 'example command',
     async execute(bot, message, args) {
-        bot.chat(`username: ${bot.username} //`)
+        const revHash = execSync("git rev-parse --short HEAD")
+            .toString()
+            .trim()
+        const remote  = execSync("git config --get remote.origin.url")
+            .toString()
+            .trim()
+            .replace("https://", "") // no embed 
+            .replace(".git", "")
+
+        bot.chat(`username: &a${bot.username}&f ○ commit &a${revHash} &f○ &a${remote}`)
     }
 }
